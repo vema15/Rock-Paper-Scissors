ff6dbe88c7d50f19c16c79b5b1e525139721a12e
vema15 <140360041+vema15@users.noreply.github.com>
2023-10-07 21:53:50 -0700

    Removed case sensitivity for user input.

--- main.js
+++ main.js
@@ -1,75 +1,50 @@
 
 
-
+loopFunc();
+function loopFunc() {
+    for (var i = 0; i < 5; i++) { 
+        
 alert(gameFunc());
 function gameFunc() {
     
     let compChoice = compChoiceFunc();
+    console.log(compChoice);
+    
     function compChoiceFunc(compChoice) {
         let numChoice = (Math.floor(Math.random() * 3) + 1);
           if (numChoice == 1) {
-            return compChoice = `Rock`;
+            return compChoice = `rock`;
           } else if (numChoice == 2) {
-            return compChoice = `Paper`;
+            return compChoice = `paper`;
           } else if (numChoice == 3) {
-            return compChoice = `Paper`;
-          }
+            return compChoice = `scissors`;
+          } 
     }
 
 
     let userChoice = prompt(`Please enter 'Rock', 'Paper', or 'Scissors':`);
-    let winStatus = winDecider(userChoice, compChoice);
+    let standardUC = userChoice.toLowerCase();
+    console.log(standardUC);
+    let winStatus = winDecider(standardUC, compChoice);
+    console.log(winStatus);
     return winStatus
 
-    function winDecider(userChoice, compChoice, winStatus) {
-        if (userChoice == "Rock" && compChoice == "Scissors" || userChoice == "Paper" && compChoice == "Rock" || userChoice == "Scissors" && compChoice == "Paper") {   
+    function winDecider(standardUC, compChoice, winStatus) {
+        if (standardUC == "rock" && compChoice == "scissors" || standardUC == "paper" && compChoice == "rock" || standardUC == "scissors" && compChoice == "paper") {   
             return winStatus = `You have won!`;                     
-        } else if (compChoice == "Rock" && userChoice == "Scissors" || compChoice == "Paper" && userChoice == "Rock" || compChoice == "Scissors" && userChoice == "Paper") {
+        } else if (compChoice == "rock" && standardUC == "scissors" || compChoice == "paper" && standardUC == "rock" || compChoice == "scissors" && standardUC == "paper") {
             return winStatus = `You lose, please try again!`;
-        } else if (userChoice == compChoice) {
+        } else if (standardUC == compChoice) {
             return winStatus = `You have tied!`;
         }
     }
 
 
 }
+}
+
+}
 
 
 
 
-
-/*gameFunc();
-function gameFunc() {
-        const userInput = prompt("Please enter 'Rock', 'Paper' or 'Scissors':");
-        innerFunc(userInput);    
-        function innerFunc(userInput) {
-                let compChoice;
-                let numChoice = (Math.floor(Math.random() * 3)+1);
-                    if (numChoice === 1) {
-                         compChoice = "Rock";
-                    } else if (numChoice === 2) {
-                         compChoice = "Paper";
-                    } else if (numChoice === 3) {
-                         compChoice = "Scissors";
-                    }
-
-                    winDecider(userInput, compChoice);
-                    console.log(winDecider(userInput,compChoice));
-
-                function winDecider(userInput, compChoice) {           
-                   let winStatus;
-                    if (userInput == "Rock" && compChoice == "Scissors" || userInput == "Paper" && compChoice == "Rock" || userInput == "Scissors" && compChoice == "Paper") {                        
-                    } else if (compChoice == "Rock" && userInput == "Scissors" || compChoice == "Paper" && userInput == "Rock" || compChoice == "Scissors" && userInput == "Paper") {
-                    } else if (userInput == compChoice) {
-                    }
-                }
-            }            
-           
-
-
-
-    
-
-        }
-
-*/
