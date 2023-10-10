b01137824f23ef01058725541901f6391fdf6622
vema15 <140360041+vema15@users.noreply.github.com>
2023-10-09 18:08:06 -0700

    Created 'start' and 'reset' button functionality. Win values are stored.

--- main.js
+++ main.js
@@ -1,43 +1,54 @@
+
 let result = [];
-console.log(result);
 
-loopFunc();
-function loopFunc() {
-    for (var i = 0; i < 5; i++) { 
-        
-gameFunc();
-function gameFunc() {
+let btn_1 = document.getElementById('button-1');
+btn_1.addEventListener('click', () => gameFunc());
+        function gameFunc() {
+
+            let compChoice = compChoiceFunc();
+            function compChoiceFunc(compChoice) {
+                let numChoice = (Math.floor(Math.random() * 3) + 1);
+                if (numChoice == 1) {
+                    return compChoice = `rock`;
+                } else if (numChoice == 2) {
+                    return compChoice = `paper`;
+                } else if (numChoice == 3) {
+                    return compChoice = `scissors`;
+                }
+            }
+
+
+            let userChoice = prompt(`Please enter 'Rock', 'Paper', or 'Scissors':`);
+            let standardUC = userChoice.toLowerCase();
+            let winStatus = winDecider(standardUC, compChoice);
+            result.unshift(winStatus);
+            
+
+            function winDecider(standardUC, compChoice, winStatus) {
+                if (standardUC == "rock" && compChoice == "scissors" || standardUC == "paper" && compChoice == "rock" || standardUC == "scissors" && compChoice == "paper") {
+                    return winStatus = `You have won!`;
+                } else if (compChoice == "rock" && standardUC == "scissors" || compChoice == "paper" && standardUC == "rock" || compChoice == "scissors" && standardUC == "paper") {
+                    return winStatus = `You lose, please try again!`;
+                } else if (standardUC == compChoice) {
+                    return winStatus = `You have tied!`;
+                }
+            }
+
+            
+            document.getElementById("comp_result").innerHTML = compChoice.toUpperCase();
+            document.getElementById("player_result").innerHTML = userChoice.toUpperCase();
+            document.getElementById("match_result").innerHTML = winStatus.toUpperCase();
     
-    let compChoice = compChoiceFunc();    
-    function compChoiceFunc(compChoice) {
-        let numChoice = (Math.floor(Math.random() * 3) + 1);
-          if (numChoice == 1) {
-            return compChoice = `rock`;
-          } else if (numChoice == 2) {
-            return compChoice = `paper`;
-          } else if (numChoice == 3) {
-            return compChoice = `scissors`;
-          } 
-    }
-
-
-    let userChoice = prompt(`Please enter 'Rock', 'Paper', or 'Scissors':`);
-    let standardUC = userChoice.toLowerCase();
-    let winStatus = winDecider(standardUC, compChoice);
-    result.unshift(winStatus);
-
-    function winDecider(standardUC, compChoice, winStatus) {
-        if (standardUC == "rock" && compChoice == "scissors" || standardUC == "paper" && compChoice == "rock" || standardUC == "scissors" && compChoice == "paper") {   
-            return winStatus = `You have won!`;                     
-        } else if (compChoice == "rock" && standardUC == "scissors" || compChoice == "paper" && standardUC == "rock" || compChoice == "scissors" && standardUC == "paper") {
-            return winStatus = `You lose, please try again!`;
-        } else if (standardUC == compChoice) {
-            return winStatus = `You have tied!`;
+            
+            console.log(result);
+       
         }
-    }
+    
+let btn_2 = document.getElementById('button-2');
+btn_2.addEventListener('click', ()=> resetFunc());
+function resetFunc() {
+    location.reload();
+}
 
 
-}
-}
 
-}
